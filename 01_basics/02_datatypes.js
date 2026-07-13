8e7820601f77125dc9086449e1c5605eda91a8e6
Calwin Fernandes <157140664+calwin2499@users.noreply.github.com>
2026-07-13 04:43:17 +0000

    Datatypes of JS

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" /* Treat all the JS code as a new version. 
+Even if the code is of previous version using this command will make use of the new version
+and u can use the new version of code as a new developer in a old code base*/
+
+// alert(3+3)   //we are using nodejs instead of browser so it can't give the out put as 6! 
+
+console.log(3
+    +3)   //This is wrong the code should be readable 
+
+//ecma script is the standard documantation and mdn is also another type of documantation
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null);        //object
+console.log(typeof undefined);   //undefined
+
