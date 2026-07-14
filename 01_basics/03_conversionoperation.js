9e74803e14a7e0ab63eb7b2a833f156692534af3
Calwin Fernandes <157140664+calwin2499@users.noreply.github.com>
2026-07-14 10:50:25 +0000

    conversion and operations done

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -23,3 +23,32 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log( typeof stringNumber);
+
+
+//************************** Operations ********************************
+
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)                           These are the basic operations made in js
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello";
+let str2 = " calwin";
+let str3 = str1 + str2 ;
+console.log(str3);
+
+
+console.log( "1" + 2 );
+console.log( 1 + "2" );
+console.log( "1" + 2 + 2);
+console.log( 1 + 2 + "2" );
+
+
