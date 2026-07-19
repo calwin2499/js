b75a16755ca6e3c455cea1de57aae8a11ecb543a
Calwin Fernandes <157140664+calwin2499@users.noreply.github.com>
2026-07-19 15:12:09 +0000

    Basics of Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,6 @@
+const name = "calwin"
+const repoCount = 50
+
+// console.log(name + repoCount +" value");     not to write like this outdated !!!
+
+console.log(`Hello my Name is ${name} and my repo count is ${repoCount}`);
