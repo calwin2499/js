065020019386e7c34fe56e8701a6b9babf0273ef
Calwin Fernandes <157140664+calwin2499@users.noreply.github.com>
2026-07-20 10:09:22 +0000

    Strigs Execution in detail

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,4 +3,32 @@ const repoCount = 50
 
 // console.log(name + repoCount +" value");     not to write like this outdated !!!
 
+// This is the actul way to write the code !!!
+
 console.log(`Hello my Name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Calwin')
+console.log(gameName);
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.indexOf('w'));
+console.log(gameName.charAt(3));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "  Calwin  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://calwin.com//calwin%20fernandes"
+console.log(url.replace('%20', ' '));
