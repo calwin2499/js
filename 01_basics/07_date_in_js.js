522789f21d2542966bbc5317836bfafba76ad1f7
Calwin Fernandes <157140664+calwin2499@users.noreply.github.com>
2026-07-22 00:46:06 +0000

    date topic in js and how to use it effciently

--- 01_basics/07_date_in_js.js
+++ 01_basics/07_date_in_js.js
@@ -10,3 +10,24 @@ console.log(myCreatedDate.toDateString());
 let myCreatedDates = new Date(2026, 0, 23, 5, 3)
 console.log(myCreatedDates.toLocaleString());
 
+// let myCreatedDateS = new Date("2026-01-23")
+let myCreatedDateS = new Date("01-23-2026")
+console.log(myCreatedDateS.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDateS.getTime());
+
+console.log(Math.floor( Date.now() / 1000));
+
+
+let DATE = new Date();
+let indiaTimes = DATE.toLocaleString('en-IN', { timeZone: 'Asia/Kolkata'});
+console.log(indiaTimes);
+
+let indiaTime = new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata'});
+console.log(indiaTime);
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
